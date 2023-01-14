a3089eba039738b38f35baf0b3d40d9e88ff8f07
Esteban <cddanguez2001@gmail.com>
2023-01-14 12:50:52 -0600

    Tenemos ya la busqueda de pokemon y muestra de lista de los personajes, falta edicion para que se vea bien

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,68 @@
-const btn = document.getElementById('btnEnviar');
-const div = document.getElementById('clone');
+const btn = document.getElementById("btnEnviar");
+const div = document.getElementById("clone");
 
-btn.addEventListener('click', ()=>{
-  console.log('pokemon encontrado');
-  
-  const url= 'https://pokeapi.co/api/v2/pokemon/pikachu/';
-  fetch(url)
-  .then((respo) => respo.json())
-  //.then((data) => console.log(data));
-  .then((dato) => {
-    listado(dato);
-  });
+const input = document.getElementById("evento");
+const tbody = document.getElementById('nombresForm');
 
+btn.addEventListener("click", () => {
+  event.preventDefault();
+  console.log(input.value);
+  busqueda(input.value);
 });
 
-function listado (datos) {
+function busqueda(pokemons) {
+  console.log("pokemon encontrado");
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemons}/`; 
+  fetch(url)
+    .then((respo) => respo.json())
+    //.then((data) => console.log(data))
+    .then((dato) => {listado(dato);})
+    
+}
 
-    const img = document.createElement('img');
-    img.src = datos.sprites.front_default;
+function listado(datos) {
+  const img = document.createElement("img");
+  img.src = datos.sprites.front_default;
 
-    const p = document.createElement('div');
-    p.innerHTML = `
-    <p>${datos.name}</p>
+  const p = document.createElement("div");
+  p.innerHTML += `
     <img src="${datos.sprites.front_default}" alt="">
-    `
-    div.appendChild(p);
+    <p>${datos.name}</p>
+    <p>${datos.abilities.ability}</p>
+    `;
+  div.appendChild(p);
+}
 
 
+function mostrar (){  //mostrara el contenido directamente al iniciar el DOM
+ 
+  const urls = `https://pokeapi.co/api/v2/pokemon/pikachu/`;
+  fetch(urls)
+  .then((respons) => respons.json())
+  
+ //.then((dom) => console.log(dom))
+  .then((dom) => {lista(dom);})
+  
+}
+mostrar();
+
+
+function lista (e){
+  
+    
+    const th = document.createElement('p');
+    const img = document.createElement("img");
+   img.src = e.sprites.front_default;
+   console.log('todo bien');
+   
+  th.innerHTML += `
+            <tr>
+              <td>${e.name}</td>
+              <td><img src="${e.sprites.front_default}" alt=""></td>
+            </tr>
+              `
+  tbody.appendChild(th);
+    
+  ;
+  
 }
