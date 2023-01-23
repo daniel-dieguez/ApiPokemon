dea499b204088c8bf77c3e1f1d0444070a27e150
Esteban <cddanguez2001@gmail.com>
2023-01-23 16:27:44 -0600

    muestra experiencia e imagen en busqueda y ya muestra los 1279 pokemons

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ const div = document.getElementById("clone");
 
 const input = document.getElementById("evento");
 const tbody = document.getElementById('nombresForm');
+const btnbuscar = document.getElementById('btnBuscar');
 
 btn.addEventListener("click", () => {
   event.preventDefault();
@@ -27,42 +28,45 @@ function listado(datos) {
   const p = document.createElement("div");
   p.innerHTML += `
     <img src="${datos.sprites.front_default}" alt="">
-    <p>${datos.name}</p>
-    <p>${datos.abilities.ability}</p>
+    <p>Nombre:
+    ${datos.name}</p>
+    <p>Experiencia:
+    ${datos.base_experience}</p>
     `;
   div.appendChild(p);
 }
 
 
-function mostrar (){  //mostrara el contenido directamente al iniciar el DOM
- 
-  const urls = `https://pokeapi.co/api/v2/pokemon/pikachu/`;
-  fetch(urls)
-  .then((respons) => respons.json())
-  
- //.then((dom) => console.log(dom))
-  .then((dom) => {lista(dom);})
-  
+const mostrar = async ()=>{
+  try{
+    const URL = `https://pokeapi.co/api/v2/pokemon/?offset=1&limit=1279`;
+    const api = await fetch(URL);
+    const data = await api.json();
+    const datapokemon= data.results.map(datapokem => datapokem);
+    //const datos = data.name.map(datopoken => datopoken);
+    //console.log(datos);
+    console.log(datapokemon);
+    lista(datapokemon);
+
+
+  }catch(error){
+    console.log(error);
+
+  }
 }
 mostrar();
 
 
-function lista (e){
-  
-    
-    const th = document.createElement('p');
-    const img = document.createElement("img");
-   img.src = e.sprites.front_default;
-   console.log('todo bien');
-   
-  th.innerHTML += `
+
+function lista (item){
+  const p = document.createElement('p');
+  p.innerHTML = `
             <tr>
-              <td>${e.name}</td>
-              <td><img src="${e.sprites.front_default}" alt=""></td>
+              <td>${item.name}</td>
             </tr>
-              `
-  tbody.appendChild(th);
-    
-  ;
-  
+  `;
+  tbody.appendChild(p);
+
 }
+
+
